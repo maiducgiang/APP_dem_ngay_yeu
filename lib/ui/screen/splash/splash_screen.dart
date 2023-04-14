9aacd89da1ce84168c87c8818b6f535d491add6b
TamHM <minhtamhoang207@gmail.com>
2023-04-15 03:09:41 +0700

    resolve conflict

--- lib/ui/screen/splash/splash_screen.dart
+++ lib/ui/screen/splash/splash_screen.dart
@@ -2,10 +2,11 @@ import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:mubaha/ui/router/router.gr.dart';
-import 'package:mubaha/ui/services/account_services.dart';
 import 'package:mubaha/ui/theme/app_path.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../sign_up/sign_up_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -37,7 +38,7 @@ class _SplashScreenState extends State<SplashScreen> {
     await Future.delayed(const Duration(seconds: 1));
     context.router.pushAll([
       _remoteConfig.getBool('show_sign_up')
-        ? const SignUpScreen()
+        ? const SignUp()
         : const MainPage()
     ]);
   }
