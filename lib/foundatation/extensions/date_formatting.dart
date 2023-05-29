bee2547c2529195f65ccac05f5f5791d00fcb545
Mabook Pro <>
2023-05-30 00:35:04 +0700

    update

--- lib/foundatation/extensions/date_formatting.dart
+++
@@ -1,8 +0,0 @@
-import 'package:intl/intl.dart';
-
-extension DateFormatting on DateTime {
-  String tohhmmddMMyyyy() {
-    final f = DateFormat('hh:mm dd/MM/yyyy');
-    return f.format(this);
-  }
-}
--- lib/foundatation/extensions/string_formatting.dart
+++
@@ -1,6 +0,0 @@
-extension StringFormatting on String {
-  String formatPhoneNumber() {
-    if (this.contains('+84')) return this.replaceAll('+84', '0');
-    return this;
-  }
-}
